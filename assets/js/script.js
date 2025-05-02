78d66132bab7e71d3878a559d70c33c5dcfdab36
LandHomesTexas (Unofficial) <lhtengineeringlibrary@gmail.com>
2025-05-03 02:46:37 +0800

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const callsUrl = "https://script.google.com/macros/s/AKfycbyGnnjQDUxrCtc8us-b5MW_EFG5uHcyc95ayIx6IqN6ka1fA-ChGIOFiD_yHWpj9xTmnQ/exec";
+const textsUrl = "https://script.google.com/macros/s/AKfycbxvBAlvnGl46E74y7nNoNIqBiB1qyuFuEx0_etB7xYK_7geGWijflvCMiC9mfbPUZAO_g/exec"
 
 fetch(callsUrl)
     .then(res => res.json())
@@ -7,3 +8,11 @@ fetch(callsUrl)
         document.getElementById("callsReceived").textContent = data["Calls Received"];
     })
     .catch(err => console.error("Failed to load Calls data:", err));
+
+fetch(textsUrl)
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById("textsSent").textContent = data["Texts Sent"];
+        document.getElementById("textsReceived").textContent = data["Texts Received"];
+    })
+    .catch(err => console.error("Failed to load Texts data:", err));
